2ec8c8630f8c8b53fb2ae810976e4f23ef13fff9
aleigha23 <98423150+aleigha23@users.noreply.github.com>
2022-01-25 14:33:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("My Name Is Aleigha!")
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
